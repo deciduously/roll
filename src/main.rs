936590805f14f0550975381a8b3337d9b1bdceb9
deciduously <github@deciduously.com>
2018-05-04 12:48:23 -0400

    Split lib/main, add interactive mode

--- src/main.rs
+++ src/main.rs
@@ -1,76 +1,37 @@
-extern crate rand;
+extern crate roll;
 
-use std::{env, fmt, process, str::FromStr};
-use rand::Rng;
+use roll::{Roll, Outcome};
+use std::{env, io::{self, BufRead}};
 
-#[derive(Debug)]
-struct Roll {
-    sides: u32,
-    repeat: u32,
-}
+fn roll_strs(s: &[String]) {
+    let rolls: Vec<Roll> = s.iter().map(|i| Roll::new(i).unwrap()).collect();
 
-impl Roll {
-    fn new(s: &str) -> Result<Roll, String> {
-        let parts: Vec<&str> = s.split('d').collect();
-
-        if parts.len() != 2 {
-            Err("Not properly formatted".to_owned()) //TODO this is lazy
-        } else {
-            Ok(Roll {
-                sides: u32::from_str(parts[1]).unwrap(),
-                repeat: u32::from_str(parts[0]).unwrap(),
-            })
-        }
+    for roll in rolls {
+        println!("{}", Outcome::new(roll));
     }
 }
 
-impl fmt::Display for Roll {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}d{}", self.repeat, self.sides)
-    }
-}
+fn repl() {
+    println!("Use Ctrl-C to quit");
+    loop {
+        // TODO quit command
+        println!("Roll: ");
+        let mut line = String::new();
+        let stdin = io::stdin();
+        stdin.lock().read_line(&mut line).unwrap();
+        let each: Vec<String> = line.split_whitespace().map(|s| s.to_owned()).collect(); // EXPENSIVE, why reallocate
+        println!("{:?}", each);
 
-#[derive(Debug)]
-struct Outcome {
-    roll: Roll,
-    rolls: Vec<u32>,
-}
-
-impl Outcome {
-    fn new(roll: Roll) -> Outcome {
-        let mut rolls = Vec::new();
-        for _ in 1..(roll.repeat + 1) {
-            let result = rand::thread_rng().gen_range(1, roll.sides + 1);
-            rolls.push(result);
-        }
-        Outcome { roll, rolls: rolls }
-    }
-}
-
-impl fmt::Display for Outcome {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f,
-            "{}:\n{:?}\n{:?}\n",
-            self.roll,
-            self.rolls.iter().fold(0, |acc, x| x + acc),
-            self.rolls
-        )
+        roll_strs(&each);
     }
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() < 1 {
-        //TODO make a REPL-ish thing
-        println!("No roll provided!");
-        process::exit(1);
+    if args.len() <= 1 {
+       repl();
     }
 
-    let rolls: Vec<Roll> = args[1..].iter().map(|i| Roll::new(i).unwrap()).collect();
-
-    for roll in rolls {
-        println!("{}", Outcome::new(roll))
-    }
+    roll_strs(&args[1..]);
 }
